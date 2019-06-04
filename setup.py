260a14c8aa02646b4b2cfd12f1ef94c3672e83e3
orenovadia <orenovad@gmail.com>
2019-06-03 17:09:01 -0700

    Removed unneeded dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 here = path.abspath(path.dirname(__file__))
 
@@ -14,7 +14,6 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     all_reqs = f.read().split('\n')
 
-install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
 dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
 
 setup(
@@ -26,15 +25,15 @@ setup(
     download_url='https://github.com/orenovadia/pandas_explode/tarball/' + __version__,
     license='MIT',
     classifiers=[
-      'Development Status :: 3 - Alpha',
-      'Intended Audience :: Developers',
-      'Programming Language :: Python :: 3',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
     ],
     keywords='',
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     author='Oren Ovadia',
-    install_requires=install_requires,
+    install_requires=['pandas'],
     dependency_links=dependency_links,
     author_email='orenovad@gmail.com'
 )
