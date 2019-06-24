ed7d6e99bb3ac6b863c37cc677f37134c1054250
orenovadia <orenovad@gmail.com>
2019-06-24 08:46:05 -0700

    Unit tests package

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,18 +1,21 @@
+from unittest import TestCase
+
 import pandas as pd
+
 from pandas_explode import explode
 
 
-def test_explosion():
+class ExplodeTests(TestCase):
+    def test_row_explosion(self):
         df = pd.DataFrame({'s': ['a', 'b', 'c'], 'values': [[1, 2], [3, 4, 5], []]})
         exploded_df = explode(df, 'values')
-        assert len(exploded_df) == 5, "Unexpected row count after explosion"
+        self.assertEqual(5, len(exploded_df), "Unexpected row count after explosion")
+        self.assertEqual(list(range(1, 6)), exploded_df['values'].tolist())
 
-
-def test_col_explosion():
+    def test_col_explosion(self):
         df = pd.DataFrame({
-                's': ['a', 'b', 'c'],
-                'values': [{'col1': 1, 'col2': 2}, {'col1': 10, 'col3': 20}, {'col2': 2}],
+            's': ['a', 'b', 'c'],
+            'values': [{'col1': 1, 'col2': 2}, {'col1': 10, 'col3': 20}, {'col2': 2}],
         })
         exploded_df = explode(df, 'values', axis=1)
-        assert sorted(exploded_df.columns) == ['col1', 'col2', 'col3', 's']
-
+        self.assertSequenceEqual(['col1', 'col2', 'col3', 's'], sorted(exploded_df.columns))
